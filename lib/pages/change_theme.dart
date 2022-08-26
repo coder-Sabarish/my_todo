daf474effde2e715628f3f9263bb15d828215747
Sabarish <studysabarish@gmail.com>
2022-08-26 11:36:03 +0530

    test commit 1

--- lib/pages/change_theme.dart
+++ lib/pages/change_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class change_theme extends StatelessWidget {
-  const choose_theme({Key? key}) : super(key: key);
+  const change_theme({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
